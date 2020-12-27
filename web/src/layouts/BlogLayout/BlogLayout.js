fcd13230bb23bfd2303c9a7eb32e5398222ca75f
Obichukwu Onyeowuzoni <obichukwu.zoni@gmail.com>
2020-12-28 00:53:41 +0100

    Add Authetication

--- web/src/layouts/BlogLayout/BlogLayout.js
+++ web/src/layouts/BlogLayout/BlogLayout.js
@@ -1,6 +1,9 @@
 import { Link, routes } from '@redwoodjs/router'
+import { useAuth } from '@redwoodjs/auth'
 
 const BlogLayout = ({ children }) => {
+  const { logIn, isAuthenticated, logOut, currentUser } = useAuth()
+
   return (
     <>
       <h1>RedWood Blog</h1>
@@ -11,12 +14,33 @@ const BlogLayout = ({ children }) => {
         <li>
           <Link to={routes.about()}>About</Link>
         </li>
-        <li>
-          <Link to={routes.posts()}>Manage Posts</Link>
-        </li>
+        {isAuthenticated && (
+          <li>
+            <Link to={routes.posts()}>Manage Posts</Link>
+          </li>
+        )}
         <li>
           <Link to={routes.contact()}>Contact Us</Link>
         </li>
+        {isAuthenticated && (
+          <li>
+            <a href="#" onClick={logOut}>
+              Log Out
+            </a>
+          </li>
+        )}
+        {!isAuthenticated && (
+          <li>
+            <a href="#" onClick={logIn}>
+              Log In
+            </a>
+          </li>
+        )}
+        {isAuthenticated && (
+          <li>
+            <a href="#">{currentUser.email}</a>
+          </li>
+        )}
       </ul>
       {children}
     </>
